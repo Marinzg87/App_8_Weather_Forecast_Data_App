43b1870b2409fe370d536e6e5be072f9091f1f33
Marcin Bogusz <bogusz.marcin@icloud.com>
2024-03-10 22:48:39 +0100

    Add images.

--- backend.py
+++ backend.py
@@ -4,7 +4,7 @@ import requests
 API_KEY = os.getenv("OPEN_WEATHER_API")
 
 
-def get_data(place, days=None, option=None):
+def get_data(place, days=None):
     url = (f"http://api.openweathermap.org/data/2.5/forecast?"
            f"q={place}&"
            f"appid={API_KEY}")
@@ -13,12 +13,8 @@ def get_data(place, days=None, option=None):
     filtered_data = data["list"]
     nr_values = 8 * days
     filtered_data = filtered_data[:nr_values]
-    if option == "Temperature":
-        filtered_data = [dict["main"]['temp'] for dict in filtered_data]
-    if option == "Sky":
-        filtered_data = [dict["weather"][0]["main"] for dict in filtered_data]
     return filtered_data
 
 
 if __name__ == "__main__":
-    print(get_data(place="Tokyo"))
+    print(get_data(place="Tokyo", days=2))
